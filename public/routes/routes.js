b7d04e063b0726c6f519067942f3ab02d9010ccf
Chris Carrick <christophercarrick7@gmail.com>
2015-10-03 20:40:33 -0400

    bug

--- public/routes/routes.js
+++ public/routes/routes.js
@@ -61,4 +61,20 @@ app.config(function($stateProvider, $urlRouterProvider) {
       }
     })
 
+    .state('score', {
+      url: '/score',
+      template: `<div id="statistics">
+                     <div id="scoreboard">
+                         <h3>Correct: <span>{{ scores.correct }}</span></h3>
+                         <h3>Incorrect: <span>{{ scores.incorrect }}</span></h3>
+                     </div>
+                 </div>`,
+      controller: 'StatsController'
+    })
+
+    .state('home.newCard', {
+      url: '/new_card',
+      template: '<new-flash-card></new-flash-card>'
+    })
+
 });
